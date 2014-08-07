fd2d46a4ed681aead6b1ce3cd0ca8d3745435524
George Wu <georgewu@fb.com>
2014-08-06 19:12:12 -0700

    Initial Commit

---
+++ FaceLapseTests/FaceLapseTests.m
@@ -0,0 +1,34 @@
+//
+//  FaceLapseTests.m
+//  FaceLapseTests
+//
+//  Created by George Wu on 8/6/14.
+//  Copyright (c) 2014 George Wu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FaceLapseTests : XCTestCase
+
+@end
+
+@implementation FaceLapseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
